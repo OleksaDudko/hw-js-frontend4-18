740159747340cdcae429538cc13bb930124c9fdf
OleksaDudko <132586603+OleksaDudko@users.noreply.github.com>
2026-06-24 16:48:47 +0300

    final

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const btnEl = document.querySelector("#get-students-btn");
 const tbodyEl = document.querySelector(".tbody");
 const formEl = document.querySelector("#add-student-form");
 
-console.log(formEl);
+let currentId = null;
 
 
 // Функція для отримання всіх студентів
@@ -21,13 +21,13 @@ function getStudents() {
 function renderStudents(students) {
     const item = students.map(({id, name, age, course, skills, email, isEnrolled}) => {
     return `<tr id="${id}">
-                <td>id: ${id}</td>
-                <td>name: ${name}</td>
-                <td>age: ${age}</td>
-                <td>course: ${course}</td>
-                <td>skills: ${skills}</td>
-                <td>email: ${email}</td>
-                <td>isEnrolled: ${isEnrolled}</td>
+                <td>${id}</td>
+                <td>${name}</td>
+                <td>${age}</td>
+                <td>${course}</td>
+                <td>${skills}</td>
+                <td>${email}</td>
+                <td>${isEnrolled}</td>
                 <td>
                     <button type="button" data-action="edit">Edit</button>
                     <button type="button" data-action="delete">Delete</button>
@@ -49,11 +49,22 @@ formEl.addEventListener("submit", (e) => {
             skills : e.currentTarget.elements.skills.value,
             email : e.currentTarget.elements.email.value,
             isEnrolled : e.currentTarget.elements.isEnrolled.checked
+        };
+        if (currentId) {
+            updateStudent(currentId, data)
+            .then(getStudents)
+            .then(res => renderStudents(res))
+            .finally(() => {
+                formEl.reset()
+                currentId = null
+            })
+            return
         }
-        addStudent(data).then(() => getStudents()).then(res => renderStudents(res))
+        addStudent(data)
+        .then(() => getStudents())
+        .then(res => renderStudents(res))
         formEl.reset()
 })
-
 
 // Функція для додавання нового студента
 
@@ -65,26 +76,53 @@ function addStudent(e) {
       "Content-Type": "application/json; charset=UTF-8",
     },
   };
-  return fetch("http://localhost:3000/students",options)
+  return fetch("http://localhost:3000/students", options)
   .then(res => res.json());
 }
 
 // Функція для оновлення студента
 
-function updateStudent(id) {
-
- // твій код
-
-
-
- }
+function updateStudent(id, data) {
+    const options = {
+    method: "PUT",
+    body: JSON.stringify(data),
+    headers: {
+    "Content-Type": "application/json; charset=UTF-8",
+    },
+};
+    return fetch(`http://localhost:3000/students/${id}`, options)
+    .then(res => res.json())
+}
 
 
 
 // Функція для видалення студента
 
 function deleteStudent(id) {
-
-    // твій код
-
+    return fetch(`http://localhost:3000/students/${id}`, {
+    method: "DELETE",
+})
 }
+
+tbodyEl.addEventListener("click", (e) => {
+    const td = e.target.closest("td")
+    const currentTd = td.parentNode
+    const id = currentTd.id
+
+    if (e.target.dataset.action === "delete") { 
+        deleteStudent(id)
+        .then(getStudents)
+        .then(res => renderStudents(res))
+    }
+
+    if (e.target.dataset.action === "edit") {
+        currentId = id
+
+        formEl.elements[0].value = currentTd.children[1].textContent
+        formEl.elements[1].value = currentTd.children[2].textContent
+        formEl.elements[2].value = currentTd.children[3].textContent
+        formEl.elements[3].value = currentTd.children[4].textContent
+        formEl.elements[4].value = currentTd.children[5].textContent
+        formEl.elements[5].checked = currentTd.children[6].textContent
+    }
+})
